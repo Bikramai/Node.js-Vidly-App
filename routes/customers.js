ad70dc00c88fc6bc056af80297cd835b637fc53d
bikramia <bikram.rhce@gmail.com>
2024-05-20 08:22:23 -0400

    Restructure Customer and Genre-models

--- routes/customers.js
+++ routes/customers.js
@@ -1,26 +1,8 @@
-const Joi = require('joi');
+const { Customer, validate } = require('../models/customer');
 const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
 
-const Customer = mongoose.model('Customer', new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        minlength: 5,
-        maxlength: 50
-    },
-    isGold: {
-        type: Boolean,
-        default: false
-    },
-    phone: {
-        type: String,
-        required: true,
-        minlength: 5,
-        maxlength: 50
-    }
-}));
 
 router.get('/', async (req, res) => {
     const customers = await Customer.find().sort('name');
@@ -77,15 +59,5 @@ router.get('/:id', async (req, res) => {
 
     res.send(customer);
 });
-
-function validateCustomer(customer) {
-    const schema = Joi.object({
-        name: Joi.string().min(5).max(50).required(),
-        phone: Joi.string().min(5).max(50).required(),
-        isGold: Joi.boolean()
-    });
-
-    return schema.validate(customer, schema);
-}
 
 module.exports = router;
